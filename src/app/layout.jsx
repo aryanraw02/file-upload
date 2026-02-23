7e87067824d3843a9389b24ab9717c49cd84c5f6
aryanraw02 <aryanraw02@gmail.com>
2026-02-23 16:45:47 +0530

    added opengraph images and fix in sitemap

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,8 +13,34 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
+  metadataBase: new URL("https://file-upload-psi-five.vercel.app"),
+
   title: "FileShare",
-  description: "A simple file upload app built with Next.js",
+  description: "A simple and secure file upload app built with Next.js",
+
+  openGraph: {
+    title: "FileShare",
+    description: "A simple and secure file upload app built with Next.js",
+    url: "https://file-upload-psi-five.vercel.app",
+    siteName: "FileShare",
+    images: [
+      {
+        url: "/cloud.png",
+        width: 1200,
+        height: 630,
+        alt: "FileShare App Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "FileShare",
+    description: "A simple and secure file upload app built with Next.js",
+    images: ["/cloud.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,7 +1,7 @@
 export default function Sitemap()  {
     return [
         {  
-            url: 'https://file-system-xi.vercel.app/',
+            url: 'https://file-upload-psi-five.vercel.app',
             lastModified: new Date(),
             changeFrequency: 'yearly',
             priority: 1.0,
